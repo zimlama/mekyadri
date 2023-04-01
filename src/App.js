3244346981841751078cd4202ae56539343918ff
zimlama <zimlama@gmail.com>
2023-04-01 10:13:50 -0500

    comment (LocalExperiences)

--- src/App.js
+++ src/App.js
@@ -36,9 +36,8 @@ function App() {
           {/* <Route exact path="/activities" element={<Reservations/>}/> */}
           <Route exact path="/shoppingcart" element={<ShoppingCart />} />
           <Route exact path="/modify" element={<Modify />} />
-          <Route exact path="/activities" element={<LocalExperiences />} />
+          {/* <Route exact path="/activities" element={<LocalExperiences />} /> */}
           <Route exact path="/createcomment" element={<CreateComent />} />
-            
           <Route exact path="/pending" element={<Pending />} />
           <Route exact path="/success" element={<Success />} />
           <Route exact path="/fail" element={<Error />} />
